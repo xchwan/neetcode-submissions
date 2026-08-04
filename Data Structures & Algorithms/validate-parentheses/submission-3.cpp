d7a2a7c88ea2a1c4250f7105822e185ae73a5b86
Chao-Hung Wan <qchwan@gmail.com>
2026-08-04 11:57:01 +0800

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+
+        if (s.size() % 2 != 0) {
+            return false;
+        }
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                stk.push(s[i]);
+            } else if (s[i] == ')') {
+                if (!stk.empty() && stk.top() == '(') {
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            } else if (s[i] == '}') {
+                if (!stk.empty() && stk.top() == '{') {
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            } else if (s[i] == ']') {
+                if (!stk.empty() && stk.top() == '[') {
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stk.empty();
+    }
+};
