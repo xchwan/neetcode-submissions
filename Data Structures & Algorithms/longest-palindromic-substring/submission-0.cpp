ac59657bc841a633887f01be622451bc87d16335
Chao-Hung Wan <qchwan@gmail.com>
2026-08-17 14:39:09 +0800

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int resIdx = 0, resLen = 0;
+        int n = s.size();
+
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                if (s[i] == s[j] && (j - i <= 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+
+                    if (resLen < (j - i + 1)) {
+                        resIdx = i;
+                        resLen = j - i + 1;
+                    }
+                }
+            }
+        }
+
+        return s.substr(resIdx, resLen);
+    }
+};
