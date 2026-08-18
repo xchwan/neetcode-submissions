f7c98b57d09f8e8d7cd04ef8442a07a46ec04612
Chao-Hung Wan <qchwan@gmail.com>
2026-08-18 13:12:29 +0800

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+
+        if (s[0] == '0' || n == 0) return 0;
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            if (s[i - 1] != '0') {
+                dp[i] += dp[i - 1];
+            }
+            int twoDigit = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
+
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+};
