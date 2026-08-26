450005300974d9afbdba1562845a92bbac81b049
Chao-Hung Wan <qchwan@gmail.com>
2026-08-26 15:32:21 +0800

    Add: unique-paths-ii - submission-2

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) break;
+                dp[0][i] = 1;
+        }
+
+
+        for (int j = 0; j < m; j++) {
+            if (obstacleGrid[j][0] == 1) break;
+            dp[j][0] = 1;
+        }
+
+        for (int j = 1; j < m; j++) {
+            for (int i = 1; i < n; i++) {
+                if(obstacleGrid[j][i] != 1) {
+                    dp[j][i] = dp[j - 1][i] + dp[j][i - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+};
